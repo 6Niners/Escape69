7cb51172d35bb4d302ea7c69ab7bab736251fb37
Mohamed-H-Anwar <mohamedpc.anwar@gmail.com>
2020-05-01 02:14:28 +0200

    Initialization of Base_Game

---
+++ BuildingEscape/Source/BuildingEscape/Grabber.cpp
@@ -0,0 +1,136 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Grabber.h"
+
+// Sets default values for this component's properties
+UGrabber::UGrabber()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+
+// Called when the game starts
+void UGrabber::BeginPlay()
+{
+	Super::BeginPlay();
+	AttachInputComponent();
+	AttachPhysicsHandle();
+}
+
+
+// Called every frame
+void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	if (!PhysicsHandle) {
+		return;
+	}
+	else if (PhysicsHandle->GetGrabbedComponent()) {
+
+		PhysicsHandle->SetTargetLocation(ReturnTraceLineEnd());
+	}
+}
+
+
+
+void UGrabber::AttachInputComponent() {
+
+	InputComponent = GetOwner()->FindComponentByClass<UInputComponent>();
+
+	if (InputComponent) {
+
+		InputComponent->BindAction("Grab", EInputEvent::IE_Pressed, this, &UGrabber::Grab);
+		InputComponent->BindAction("Grab", EInputEvent::IE_Released, this, &UGrabber::Release);
+	}
+}
+
+
+
+void UGrabber::AttachPhysicsHandle() {
+
+	PhysicsHandle = GetOwner()->FindComponentByClass<UPhysicsHandleComponent>();
+
+	if (!PhysicsHandle) {
+
+		UE_LOG(LogTemp, Error, TEXT("%s is missing a physics component"), *(GetOwner()->GetName()))
+	}
+}
+
+
+
+void UGrabber::Grab() {
+
+	// reference to Hit object
+	FHitResult HitResult = ReturnFirstTracePhysicsObject();
+	UPrimitiveComponent* ComponentToGrab = HitResult.GetComponent();
+	AActor* HitActor = HitResult.GetActor();
+
+	//if Physics Object is hit
+	if (HitActor) {
+		//grab component
+		PhysicsHandle->GrabComponent(
+			ComponentToGrab,
+			NAME_None,
+			HitActor->GetActorLocation(),
+			true
+		);
+	}		
+}
+
+
+
+void UGrabber::Release() {
+
+	if (!PhysicsHandle) {
+		return;
+	}
+	else {
+
+		PhysicsHandle->ReleaseComponent();
+	}
+}
+
+
+
+FHitResult UGrabber::ReturnFirstTracePhysicsObject() {
+
+	FHitResult Hit;
+
+	bool Trace = GetWorld()->LineTraceSingleByObjectType(
+		Hit,
+		ReturnTraceLineStart(),
+		ReturnTraceLineEnd(),
+		FCollisionObjectQueryParams(ECollisionChannel::ECC_PhysicsBody),
+		FCollisionQueryParams(FName(TEXT("")), false, GetOwner())
+	);
+
+	return Hit;
+}
+
+
+
+FVector UGrabber::ReturnTraceLineStart() {
+
+	FVector ViewPointLocation;
+	FRotator ViewPointRotation;
+
+	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(ViewPointLocation, ViewPointRotation);
+	
+	return ViewPointLocation;
+}
+
+
+
+FVector UGrabber::ReturnTraceLineEnd() {
+
+	FVector ViewPointLocation;
+	FRotator ViewPointRotation;
+
+	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(ViewPointLocation, ViewPointRotation);
+
+	return ViewPointLocation + ViewPointRotation.Vector() * Reach;
+}
